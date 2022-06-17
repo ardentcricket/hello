f25cc647cd78d885619a2a9d39e6992503b8b04c
Nikhil Nambiar <nambiar.nikhil11@gmail.com>
2022-06-16 22:06:59 -0500

    Learned about slices, being able to create new spaces

--- hello.go
+++ hello.go
@@ -1,13 +1,22 @@
 package main
 
 import ("fmt"
+		"log"
 		"example.com/greetings"
 )
 
 import "rsc.io/quote"
 
 func main() {
-	fmt.Println(quote.Go())
-	message := greetings.Hello("Dwight")
-	fmt.Println(message)
+
+	log.SetPrefix("grettings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Dwight")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message, quote.Go())
 }
