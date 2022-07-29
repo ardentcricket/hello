05f6ac8e58101a85c246e14b82257ec54a40676a
Nikhil Nambiar <nambiar.nikhil11@gmail.com>
2022-07-29 07:30:21 -0500

    Create channel and block go routine

--- hello.go
+++ hello.go
@@ -1,22 +1,71 @@
 package main
 
-import ("fmt"
-		"log"
-		"example.com/greetings"
+import (
+	"fmt"
+	// "log"
+	"time"
+
+	// "example.com/greetings"
+	// "rsc.io/quote"
 )
 
-import "rsc.io/quote"
+// func main() {
+
+// 	log.SetPrefix("grettings: ")
+// 	log.SetFlags(0)
+
+// 		c := make(chan string)
+// 		go count("fish", c)
+
+// 		for msg := range c {
+// 			fmt.Println(msg)
+// 		}
+
+// 		message, err := greetings.Hello("Dwight")
+
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+	
+// 		fmt.Println(message, quote.Go())
+	
+
+// }
+
+// func count(thing string, c chan string) {
+// 	for i := 1; i <= 5; i++ {
+// 		c <- thing
+// 		time.Sleep(time.Millisecond * 500)
+// 	}
+// 	close(c)
+// }
+
+
+
+//make the channel
 
 func main() {
+	c := make(chan string)
+	go count("lion", c)
 
-	log.SetPrefix("grettings: ")
-	log.SetFlags(0)
-
-	message, err := greetings.Hello("Dwight")
-
-	if err != nil {
-		log.Fatal(err)
+	for msg := range c {
+		fmt.Println(msg)
 	}
-
-	fmt.Println(message, quote.Go())
+	fmt.Println(shouldEscape('?'))
 }
+	
+func count(thing string, c chan string) {
+		c <- thing
+		time.Sleep(time.Millisecond * 500)
+		close(c)
+
+}
+
+func shouldEscape(c byte) bool {
+    switch c {
+    case ' ', '?', '&', '=', '#', '+', '%':
+        return true
+    }
+    return false
+}
+
