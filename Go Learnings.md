99a145aabdb873534978486775049ceef78f74e4
Nikhil Nambiar <nambiar.nikhil11@gmail.com>
2022-11-27 20:05:42 -0600

    Pass by value, Pass by reference

---
+++ Go Learnings.md
@@ -0,0 +1,9 @@
+## Go Learnings
+
+- Return multiple result named parameters
+- Pass by reference, Pass by value
+    - Pass by reference changes the orginal value.
+      You pass the address (&var) to the function and changes the original value
+
+    - Pass by value does not change the actual value but creates a new one.
+      You pass a value in and it gives you a new value, and keep the orginal value intact.
--- hello.go
+++ hello.go
@@ -1,90 +1,20 @@
 package main
 
-import (
-	"fmt"
-	// "log"
-	"time"
-
-	// "example.com/greetings"
-	// "rsc.io/quote"
-)
-
-// func main() {
-
-// 	log.SetPrefix("grettings: ")
-// 	log.SetFlags(0)
-
-// 		c := make(chan string)
-// 		go count("fish", c)
-
-// 		for msg := range c {
-// 			fmt.Println(msg)
-// 		}
-
-// 		message, err := greetings.Hello("Dwight")
-
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-	
-// 		fmt.Println(message, quote.Go())
-	
-
-// }
-
-// func count(thing string, c chan string) {
-// 	for i := 1; i <= 5; i++ {
-// 		c <- thing
-// 		time.Sleep(time.Millisecond * 500)
-// 	}
-// 	close(c)
-// }
-
-
-
-//make the channel
+import "fmt"
 
 func main() {
-	c := make(chan string)
-	go count("lion", c)
-
-	for msg := range c {
-		fmt.Println(msg)
-	}
-	fmt.Println(shouldEscape('?'))
-	fmt.Println(calculator(20,5))
-}
-	
-func count(thing string, c chan string) {
-		c <- thing
-		time.Sleep(time.Millisecond * 500)
-		close(c)
-
+	a, b := 0, 0
+	value := Add(a)
+	valuePtr := AddPtr(&b)
+	fmt.Println("a: ", a, "b: ", " value for A: ", value, b, " value for B ptr:", valuePtr)
 }
 
-func shouldEscape(input byte) bool {
-	switch input {
-	case '?', '&', ' ', '=', '#', '+', '%':
-		return true
-	}
-	return false
+func Add(x int) int {
+	 x++
+	 return x
 }
 
-func unhex(c byte) byte {
-	switch {
-	case '0' <= c && c <= '9':
-		return c - '0'
-	case 'a' <= c && c <= 'f':
-		return c - 'a' + 10
-	case 'A' <= c && c <= 'F':
-		return c - 'A' + 10
-	}
-
-	return 0
-}
-
-func calculator( a int, b int) (mul int, div int) {
-	mul = a * b
-	div = a / b
-	return
+func AddPtr(x *int) int {
+	*x++
+	return *x
 }
